f83e3c464a0505c6a46cf9573870fca84dc1db49
Damien Duro <damien@weweb.io>
2022-10-20 11:58:47 +0200

    Update ww-config.js

--- ww-config.js
+++ ww-config.js
@@ -14,7 +14,7 @@ function isDataArrayObject(data) {
 export default {
     editor: {
         label: 'Chart - Scatter',
-        icon: 'fontawesome/solid/chart-scatter',
+        icon: 'logos/chartjs',
         customStylePropertiesOrder: [
             ['isLegend', 'legendPosition', 'legendAlignement', 'legendSize', 'legendColor'],
             ['axis', 'stacked', 'startAtZero', 'showLinearRegression', 'gridColor'],
