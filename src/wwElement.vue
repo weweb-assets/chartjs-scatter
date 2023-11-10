051b7cbf0aa944f0e312eada862bfa506d880b1f
Alexis JC <alexisjc.dev@gmail.com>
2023-11-10 14:17:54 +0100

    chore: QA feedbacks

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -282,9 +282,12 @@ export default {
                                 points: points.map(point => ({
                                     datasetLabel: this.chartInstance.data.datasets[point.datasetIndex].label,
                                     label: this.chartInstance.data.labels[point.index],
-                                    value: typeof this.chartInstance.data.datasets[point.datasetIndex].data[point.index] === 'object' ?
-                                        this.chartInstance.data.datasets[point.datasetIndex].data[point.index][this.content.axis === 'x' ? 'y' : 'x'] :
-                                        this.chartInstance.data.datasets[point.datasetIndex].data[point.index],
+                                    value:
+                                        typeof this.chartInstance.data.datasets[point.datasetIndex].data[
+                                            point.index
+                                        ] === 'object'
+                                            ? this.chartInstance.data.datasets[point.datasetIndex].data[point.index]['y']
+                                            : this.chartInstance.data.datasets[point.datasetIndex].data[point.index],
                                     ...point,
                                 })),
                             },
